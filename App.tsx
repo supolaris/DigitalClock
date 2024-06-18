8659a13c67c9028410d17fedc94f42e464e024df
Muhammad Suleman <msulaman061@gmail.com>
2024-06-18 17:19:40 +0500

    digital clock with dayJs library created

--- App.tsx
+++ App.tsx
@@ -1,11 +1,41 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, ImageBackground, StatusBar} from 'react-native';
+import React, {useEffect, useState} from 'react';
+
+import dayjs from 'dayjs';
 
 const App = () => {
+  const [date, setDate] = useState(dayjs());
+
+  useEffect(() => {
+    const myInterval = setInterval(() => {
+      setDate(dayjs());
+    }, 1000);
+
+    return clearInterval(myInterval);
+  }, []);
+
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <>
+      <StatusBar translucent backgroundColor="transparent" />
+      <ImageBackground
+        style={{flex: 1}}
+        source={require('./assets/wallpaper.jpg')}>
+        <View style={{alignItems: 'center', paddingTop: 60}}>
+          <Text style={{fontSize: 20, color: 'white'}}>
+            {date.format('dddd, DD MMMM')}
+          </Text>
+          <Text
+            style={{
+              fontSize: 45,
+              fontWeight: 'bold',
+              paddingTop: 10,
+              color: 'white',
+            }}>
+            {date.format('HH:mm:ss')}
+          </Text>
+        </View>
+      </ImageBackground>
+    </>
   );
 };
 
